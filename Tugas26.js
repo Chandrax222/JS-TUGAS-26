7c2dff4cf4fa44a054b68fd8aca43700a3513070
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-03 04:13:56 +0700

    isFinite
    isFinite

---
+++ Tugas26.js
@@ -0,0 +1,46 @@
+ var takterbatas = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+ 
+ var a = 0;
+
+if (2 / a) {
+    console.log("Angka 2 NOT "+2/a);
+}
+
+if (39 / a) {
+    console.log("Angka 39 NOT "+39 / a);
+}
+if (76 /a) {
+    console.log("Angka 76 NOT "+76/a);
+}
+if (50 /a ) {
+    console.log("Angka 50 NOT "+56 /a);
+}
+
+if (9 / a) {
+    console.log("ANgka 9 NOT "+9 /a);
+}
+
+if (7 /a) {
+    console.log("Angka 7 NOT "+7/a);
+}
+
+if (41 /a) {
+    console.log("Angka 41 NOT "+41/a);
+}
+
+if (2 / a) {
+    console.log("Angka 2 NOT "+2 /a);
+}
+
+if (24 / a) {
+    console.log("Angka 24 NOT "+24 /a);
+}
+
+if (1 /a) {
+    console.log("Angka 1 NOT "+1 /a);
+}
+
+if (16 /a) {
+    console.log("Angka 16 NOT "+16 /a);
+}
+
